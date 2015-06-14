2ff793e61b941eb04473d1c7134f577efb719c60
live4code <live4code@gmail.com>
2015-06-14 03:59:25 +0000

    commit exercise content by Live4Code

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function MainCtrl($scope, DataStore){
 
 myApp.factory('DataStore', function() {
   var dataToShare = {
-    message: "rats live on no evil star", 
+    message: "rats live on no evil star" 
   };
 
   var setMessage = function(message){
