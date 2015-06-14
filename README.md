eefce4d818f00118472bf5975a71473ded4151ac
live4code <live4code@gmail.com>
2015-06-14 22:21:13 +0000

    commit exercise content by Live4Code

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-###Angular Basic
+### ng-repeat
 
-Learn basic concepts and conventions of AngularJS.
+`ng-repeat` is used to show a list of items. It instantiates a template once per item from a collection. 
+Each template instance gets its own scope, where the given loop variable is set to the current collection 
+item, and `$index` is set to the item index or key.
+
+We can also add filters to the `ng-repeat` expression to further customize how to render the data. It is 
+a convenient way to contrstruct a `<table>` or `<ul>` element from a JS array.
+
+### Tracking and Duplicates
+
+By default, `ng-repeat` does not allow duplicate items in arrays. If you have duplicate items, 
+then use the track by expression.
+
+For example, you may track items by the index of each item in the collection, using the special scope property `$index`:
+
+```
+<div ng-repeat="n in [42, 42, 43, 43] track by $index">
+  {{n}}
+</div>
+``
--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 var myApp = angular.module('myApp', []);
 
 myApp.factory('Avengers', function() {
-    var Avengers = {};
-    Avengers.cast = [
-      {
-        name: "Robert Downey Jr.",
-        character: "Tony Stark / Iron Man"
-      },
-      {
-        name: "Chris Evans",
-        character: "Steve Rogers / Captain America"
-      },
-      {
-        name: "Mark Ruffalo",
-        character: "Bruce Banner / The Hulk"
-      },
-      {
-        name: "Chris Hemsworth",
-        character: "Thor"
-      },
-      {
-        name: "Scarlett Johansson",
-        character: "Natasha Romanoff / Black Widow"
-      },
-      {
-        name: "Jeremy Renner",
-        character: "Clint Barton / Hawkeye"
-      },
-      {
-        name: "Tom Hiddleston",
-        character: "Loki"
-      }
-    ];
-    return Avengers;
+  var Avengers = {};
+  Avengers.cast = [
+    {
+      name: "Robert Downey Jr.",
+      character: "Tony Stark / Iron Man"
+    },
+    {
+      name: "Chris Evans",
+      character: "Steve Rogers / Captain America"
+    },
+    {
+      name: "Mark Ruffalo",
+      character: "Bruce Banner / The Hulk"
+    },
+    {
+      name: "Chris Hemsworth",
+      character: "Thor"
+    },
+    {
+      name: "Scarlett Johansson",
+      character: "Natasha Romanoff / Black Widow"
+    },
+    {
+      name: "Jeremy Renner",
+      character: "Clint Barton / Hawkeye"
+    },
+    {
+      name: "Tom Hiddleston",
+      character: "Loki"
+    }
+  ];
+  return Avengers;
 })
 
 function SuperherosCtrl($scope, Avengers) {
-    $scope.avengers = Avengers;
+  $scope.avengers = Avengers;
 
-    //完成该方法，将选中的actor值附到$scope上，并在页面上显示出来
-    $scope.select = function(actor){
+  //finish the method, attach the actor to $scope and show it on page
+  $scope.select = function(actor){
 
 
-    };
+  };
 }
