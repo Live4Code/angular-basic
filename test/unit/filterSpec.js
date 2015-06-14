7b8395d14937118ab2337c1ad60bfbb979290cad
live4code <live4code@gmail.com>
2015-06-14 04:28:36 +0000

    commit exercise content by Live4Code

--- test/unit/filterSpec.js
+++ test/unit/filterSpec.js
@@ -4,10 +4,9 @@ describe('filter', function() {
 
 	describe('shiftString', function() {
 
-	  it('should shift string by specified number',
+	  it('string abcdefg by 2 characters should be fgabcde',
 	      inject(function(shiftStringFilter) {
 	    expect(shiftStringFilter('abcdefg',2)).toBe('fgabcde');
-	    expect(shiftStringFilter('this is a filter',3)).toBe('terthis is a fil');
 	  }));
 	});
 });
