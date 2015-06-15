ed98c2af253f1242fc2f2e213bcd0cc187c88059
live4code <live4code@gmail.com>
2015-06-15 16:10:57 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Angular Filter', function() {
+describe('filters', function() {
 
   describe('index page', function() {
 
@@ -8,19 +8,19 @@ describe('Angular Filter', function() {
       browser.get('index.html');
     });
 
-    it('should reverse order through ng-click on attributes', function() {
+    it('should reverse order through ng-click on Actor or Character twice', function() {
       element(by.model('search')).clear();
       element(by.css('a.name')).click();
       var first = element(by.repeater('actor in avengers.cast').row(0));
       var name = first.element(by.css('td:nth-child(1)')).getText();
-      expect(name.getText()).toBe('TOM HIDDLESTON');
+      expect(name.getText()).toBe('TOM HIDDLESTON', 'Reverse order by name');
       element(by.css('a.name')).click();
-      expect(name.getText()).toBe('CHRIS EVANS');
+      expect(name.getText()).toBe('CHRIS EVANS', 'Order by name');
       element(by.css('a.character')).click();
       var character = first.element(by.css('td:nth-child(2)')).getText();
-      expect(character.getText()).toBe("tony stark / iron man");
+      expect(character.getText()).toBe("tony stark / iron man", 'Reverse order by character');
       element(by.css('a.character')).click();
-      expect(character.getText()).toBe("bruce banner / the hulk");
+      expect(character.getText()).toBe("bruce banner / the hulk", 'Order by character');
     });
 
   });
