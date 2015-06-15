eaa686bd14aa43a8ccb7edca807e96e775890c36
live4code <live4code@gmail.com>
2015-06-15 01:35:08 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -8,24 +8,27 @@ describe('ng-repeat', function() {
       browser.get('index.html');
     });
 
-    it('should select actor in the table and show info in h3', function() {
+    it('should select 1st row', function() {
       element(by.model('search')).clear();
       checkRow(0);
-      checkRow(3);
     });
 
     var checkRow = function(rowIndex){
       var row = element(by.repeater('actor in avengers.cast').row(rowIndex));
       var index = row.element(by.css('td:nth-child(1)')).getText();
-      var name = row.element(by.css('td:nth-child(2)')).getText();
-      var character = row.element(by.css('td:nth-child(3)')).getText();
-      var button = row.element(by.css('.btn-primary')); 
-      expect(index).toBe((rowIndex).toString(), 'The first column in each row should be $index');
-
-      button.click();
-      var h3 = element(by.css('h3'));
-      expect(h3.getText()).toContain(name, 'h3 should contain actor name');
-      expect(h3.getText()).toContain(character, 'h3 should contain character name');
+      row.element(by.css('td:nth-child(2)')).getText().then(function(name){
+        row.element(by.css('td:nth-child(3)')).getText().then(function(character){
+          var button = row.element(by.css('.btn-primary')); 
+          expect(index).toBe((rowIndex).toString(), 'The first column in each row should be $index');
+    
+          button.click();
+          var h3 = element(by.css('h3'));
+          h3.getText().then(function(txt){
+            expect(txt).toContain(name);
+            expect(txt).toContain(character);
+          });
+        });
+      });
     };
 
   });
