b9753d2514b37ea197ef300a5f43b81bbe361b1e
live4code <live4code@gmail.com>
2015-06-19 17:20:52 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Angular Directive', function() {
+describe('Transclude', function() {
 
   describe('index page', function() {
 
@@ -8,14 +8,14 @@ describe('Angular Directive', function() {
       browser.get('index.html');
     });
 
-    it('should generate alert and panel widgets', function() {
+    it('should generate random alert and panel widgets', function() {
       var addAlertBtn = element(by.css('.add-alert'));
       element.all(by.repeater('alert in alerts')).then(function(oldRows){
         var oldLength = oldRows.length;
         addAlertBtn.click();
         element.all(by.repeater('alert in alerts')).then(function(newRows){
           var newLength = newRows.length;
-          expect(newLength).toBe(oldLength+1);
+          expect(newLength).toBe(oldLength+1, 'click add alert button should create a new alert box');
 
           var selectPanelBtn = element(by.css('.select-panel'));
           selectPanelBtn.click();
@@ -23,7 +23,7 @@ describe('Angular Directive', function() {
           var panelHead = element(by.css('.panel-heading'));
           var panelBody = element(by.css('.panel-body'));
           var panelContent = element(by.css('.panel-body div span'));
-          expect(panel.isPresent()).toBe(true);
+          expect(panel.isPresent()).toBe(true, 'click select panel button should display a random panel');
           expect(panelHead.isPresent()).toBe(true);
           expect(panelContent.getText()).toBeTruthy();
           expect(panelBody.isPresent()).toBe(true);
