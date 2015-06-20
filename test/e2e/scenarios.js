897b333444cf37dc398eca4abb15aadf1bdd5265
live4code <live4code@gmail.com>
2015-06-20 05:05:52 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -16,16 +16,14 @@ describe('Transclude', function() {
         element.all(by.repeater('alert in alerts')).then(function(newRows){
           var newLength = newRows.length;
           expect(newLength).toBe(oldLength+1, 'click add alert button should create a new alert box');
-
+          
           var selectPanelBtn = element(by.css('.select-panel'));
           selectPanelBtn.click();
           var panel = element(by.css('.panel'));
           var panelHead = element(by.css('.panel-heading'));
           var panelBody = element(by.css('.panel-body'));
           var panelContent = element(by.css('.panel-body div span'));
-          expect(panel.isPresent()).toBe(true, 'click select panel button should display a random panel');
-          expect(panelHead.isPresent()).toBe(true, 'click select panel button should display a random panel');
-          expect(panelBody.isPresent()).toBe(true, 'click select panel button should display a random panel');
+          expect(panelContent.getText()).toContain('This is a detailed','click select panel button should display a random panel');
         });
       });
     });
