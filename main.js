339d65d471d6c2627f62bdc723705b428002122a
live4code <live4code@gmail.com>
2015-06-21 22:35:53 +0000

    commit exercise content by Live4Code

--- main.js
+++ main.js
@@ -21,15 +21,19 @@ scope: {
 template: '<div class="panel panel-info">'+
   '<div class="panel-heading">Dashboard</div>'+
   '<div class="panel-body"><p>Number: {{number}}</p>'+
-  '<p>Carrier: <select ng-model="network" ng-options="net for net in networks"></select></p>' +
-  '<p><input type="text" ng-model="value" class="form-control" placeholder="Content"></p>' +
-  '<p><button class="btn btn-default" ng-click="makeCall({number: number, network:network, message: value})">'+
+  '<p>Carrier: '+
+  '<select ng-model="network" ng-options="net for net in networks">'+
+  '</select></p>'+
+  '<p>Content: '+
+  '<input type="text" ng-model="value" class="form-control"></p>'+
+  '<p><button class="btn btn-default" '+
+    'ng-click="makeCall({number: number, network:network, message: value})">'+
   'Call</button></p>'+
   '</div></div>',
 
 link: function(scope) {
   scope.networks = ["AT&T", "Verizon", "T-Mobile"];
-  scope.network = scope.networks[0]
+  scope.network = scope.networks[0];
 }
 
 };
