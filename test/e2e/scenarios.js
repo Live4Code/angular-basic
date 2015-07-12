ff23fc3e6e59483230c85170ca5d24c363aeaf47
live4code <live4code@gmail.com>
2015-07-12 08:34:57 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -16,7 +16,7 @@ describe('Promise', function() {
       expect(foodResult.getText()).toContain('Pizza', 'Click the first button should show Pizza in h3');
 
       element(by.css('.menu div:nth-child(3) button')).click();
-      expect(foodResult.getText()).toBe('Vegetable', 'Click the third button should show Vegetable in h3');
+      expect(foodResult.getText()).toContain('Vegetable', 'Click the third button should show Vegetable in h3');
 
       element(by.css('.menu div:nth-child(4) button')).click();
       expect(noFoodResult.isDisplayed()).toBe(true, 'Click no food button should show no dinner message');
