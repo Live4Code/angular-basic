b9406bdc87b7c66891293398ee3a0618d5ec1176
live4code <live4code@gmail.com>
2015-07-12 08:24:06 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,26 +1,25 @@
 'use strict';
 
-describe('Angular Promise', function() {
+describe('Promise', function() {
 
-  describe('index page', function() {
+  describe('food tonight', function() {
 
     beforeEach(function() {
       browser.get('index.html');
     });
 
-    it('should show food on page', function() {
+    it('should show food clicking first 3 buttons and show no food message clicking the last button', function() {
       var foodResult = element(by.css('.food'));
       var noFoodResult = element(by.css('.no-food'));
 
       element(by.css('.menu div:nth-child(1) button')).click();
-      expect(foodResult.getText()).toBe('今晚的食物是麦当劳');
+      expect(foodResult.getText()).toContain('Pizza', 'Click the first button should show Pizza in h3');
 
       element(by.css('.menu div:nth-child(3) button')).click();
-      expect(foodResult.getText()).toBe('今晚的食物是小肥羊');
+      expect(foodResult.getText()).toBe('Vegetable', 'Click the third button should show Vegetable in h3');
 
       element(by.css('.menu div:nth-child(4) button')).click();
-      expect(foodResult.isDisplayed()).toBeFalsy();
-      expect(noFoodResult.isDisplayed()).toBeTruthy();
+      expect(noFoodResult.isDisplayed()).toBe(true, 'Click no food button should show no dinner message');
     });
 
   });
